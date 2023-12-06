aeed3d37c73f89d54ad0c6e12c7be42b0cf82345
Cristian morales <ccmj202@gmail.com.com>
2023-12-05 22:25:26 -0500

    🚀 Home: New Home page

---
+++ components/FullLogo.vue
@@ -0,0 +1,15 @@
+<template>
+  <img class="moracrisLogo" src="~/assets/moraCrisFullLogo.png"/>
+</template>
+<style scoped>
+.moracrisLogo {
+  animation: 3s appear;
+  margin: auto;
+}
+
+@keyframes appear {
+  0% {
+    opacity: 0;
+  }
+}
+</style>
